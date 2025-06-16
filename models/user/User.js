9e1e3ea7b97ccc77c14d7c5dbb804f8ded823a59
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2025-06-16 09:18:52 +0200

    add role

--- models/user/User.js
+++ models/user/User.js
@@ -6,6 +6,11 @@ const schema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     tel: { type: String, required: false },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user"
+    }
   },
   {
     timestamps: true,
